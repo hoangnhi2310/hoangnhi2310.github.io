ef6bf09298f862aad2cc79fa20a2f6682c44bea8
Nhi Le <hoangnhi2310999@gmail.com>
2020-07-24 19:33:26 +0700

    JPG

--- giaydep_classification.py
+++ giaydep_classification.py
@@ -11,7 +11,7 @@ data_dir = tf.keras.utils.get_file(origin=dataset_url,
                                    fname='giaydep', 
                                    untar=True)
 data_dir = pathlib.Path(data_dir)
-image_count = len(list(data_dir.glob('*/*.jpg')))
+image_count = len(list(data_dir.glob('*/*.JPG')))
 print(image_count)
 #%%
 giay = list(data_dir.glob('giay/*'))
@@ -23,13 +23,13 @@ np.set_printoptions(linewidth=200)
 PIL.Image.open(str(giay[1]))
 #%%
 
-batch_size = 32
-img_height = 180
-img_width = 180
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+# batch_size = 32
+# img_height = 180
+# img_width = 180
+# train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+#   data_dir,
+#   validation_split=0.2,
+#   subset="training",
+#   seed=123,
+#   image_size=(img_height, img_width),
+#   batch_size=batch_size)
