a931392f17a5b73c03e51ebb1c2f562dfa6c9c8e
Nhi Le <hoangnhi2310999@gmail.com>
2020-07-28 20:50:20 +0700

    zip file

--- giaydep_classification.py
+++ giaydep_classification.py
@@ -6,13 +6,73 @@ Created on Fri Jul 24 15:02:46 2020
 """
 import tensorflow as tf
 import pathlib
-dataset_url = "https://hoangnhi2310.github.io/giaydep.tar"
+dataset_url = "https://hoangnhi2310.github.io/giaydep2.tar"
 data_dir = tf.keras.utils.get_file(origin=dataset_url, 
                                    fname='giaydep', 
                                    untar=True)
 data_dir = pathlib.Path(data_dir)
-image_count = len(list(data_dir.glob('*/*')))
-print(image_count)
+# image_count = len(list(data_dir.glob('*/*')))
+# print(image_count)
+#%% load from local
+import tensorflow as tf
+import pathlib
+# path = "D:\hoangnhi2310.github.io\hoangnhi2310.github.io\giaydep2.tar"
+path = r"D:\Hahalolo\persona\test_images\Giày dep\giaydep.tar"
+data_dir = tf.keras.utils.get_file(origin=path, 
+                                   fname='giaydep', 
+                                   untar=True)
+data_dir = pathlib.Path(data_dir)
+#%%
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(
+    data_dir,
+    target_size=(300,300),
+    batch_size=128,
+    class_mode='binary'
+)
+#%%
+import cv2
+import glob
+count = 1
+path_dep = 'giay/*'
+path_giay = 'dep/*'
+paths_list = [path_dep, path_giay]
+for path in paths_list:
+    filenames = data_dir.glob(path)
+    for filename in filenames:
+      image = cv2.imread(filename)
+      gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+      image = cv2.resize(image, (128, 128))
+      gray_img = cv2.resize(gray_img, (128, 128))
+      cv2.imwrite("gray_images/gray_" +str(count) +".jpg", gray_img)
+      cv2.imwrite("color_images/color_" +str(count) +".jpg", image)
+      count += 1
+#%%
+from keras.preprocessing import image
+test_image= image.load_img(r"D:\Hahalolo\persona\test_images\5.jpg") 
+test_image = image.img_to_array(test_image)
+test_image = tf.image.rgb_to_grayscale(test_image, name=test_image)
+#%%
+path_local = r"giay\IMG_6338.JPG"
+# filename = glob.glob('giay/IMG_6338.JPG')
+image = cv2.imread(path_local, 0)
+cv2.imshow('image', image)
+image = cv2.imread(filename)
+gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.resize(image, (128, 128))
+gray_img = cv2.resize(gray_img, (128, 128))
+cv2.imwrite("gray_images/gray_" +str(count) +".jpg", gray_img)
+cv2.imwrite("color_images/color_" +str(count) +".jpg", image)
+count += 1
+#%%
+path_test = r"D:\\nền\\bia.jpeg"
+image = cv2.imread(path_test, 0)
+cv2.imshow('image', image)
+print(image.shape)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 #%%
 giay = list(data_dir.glob('giay/*'))
 import os
@@ -36,7 +96,8 @@ PIL.Image.open(str(giay[1]))
 #%% load from local
 import tensorflow as tf
 import pathlib
-path = "D:\hoangnhi2310.github.io\hoangnhi2310.github.io\giaydep.tar"
+# path = "D:\hoangnhi2310.github.io\hoangnhi2310.github.io\giaydep2.tar"
+path = "D:\Hahalolo\persona\test_images\Giày dep\giaydep.tar"
 data_dir = tf.keras.utils.get_file(origin=path, 
                                    fname='giaydep', 
                                    untar=True)
