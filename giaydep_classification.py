c8f923494ad2cb148452176f1388bbcca8381127
Nhi Le <hoangnhi2310999@gmail.com>
2020-07-29 09:19:45 +0700

    upload new colab

--- giaydep_classification.py
+++ giaydep_classification.py
@@ -23,7 +23,6 @@ data_dir = tf.keras.utils.get_file(origin=path,
                                    untar=True)
 data_dir = pathlib.Path(data_dir)
 #%%
-import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 train_datagen = ImageDataGenerator(rescale=1./255)
 train_generator = train_datagen.flow_from_directory(
